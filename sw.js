08bb735a277be2b48519f07c671cbd01272a6ad4
donge7 <130029339+donge7@users.noreply.github.com>
2026-04-23 11:03:23 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'wm-v1';
+const ASSETS = [
+  './weight-manager.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
